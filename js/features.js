6d0aa7880c196e9a2afa376dc6873b0d5e46090a
Md Abir Hasan <tabir085@gmail.com>
2024-09-25 01:04:25 +0600

    file system added 7

--- js/features.js
+++ js/features.js
@@ -21,11 +21,7 @@ document.getElementById('history-btn').addEventListener('click',function(event){
 
 document.getElementById('btn-blog').addEventListener('click',function(event){
     event.preventDefault();
-    window.location.href ="https://abir-hasan59.github.io/assingnment-5/blog.html";
+    window.location.href ="blog.html";
 });
 
-document.getElementById('btn-blog').addEventListener('click',function(event){
-    event.preventDefault();
-    window.location.href ="index.html";
-});
 
